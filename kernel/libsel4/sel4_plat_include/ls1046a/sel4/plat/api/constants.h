88e9c20bddd168ae7e37683bd1737ef28b38032a
colored-dye <baoyuntai@outlook.com>
2023-02-28 08:50:05 +0800

    Patched libsel4; overlay for /memory/reg

---
+++ kernel/libsel4/sel4_plat_include/ls1046a/sel4/plat/api/constants.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright (C) 2021, Hensoldt Cyber GmbH
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#pragma once
+
+#include <autoconf.h>
+/* 4x Cortex-A72. */
+#include <sel4/arch/constants_cortex_a72.h>
